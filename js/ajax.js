039245178df1ee557b408f6332b1040059fa78ee
makounek <resj01@vse.cz>
2020-06-03 04:26:26 +0200

    lokalni prihlasovani a registrovani hotovo

--- js/ajax.js
+++ js/ajax.js
@@ -45,7 +45,7 @@ $(function(){
 		} else {
 
 			$.ajax({
-	            url :'./register.php',
+	            url :'./backends/register.php',
 	            type:'POST',
 	            data :{
 	            'name':name,
@@ -104,7 +104,7 @@ $(function(){
 		} else {
 
 			$.ajax({
-	            url :'./login.php',
+	            url :'./backends/login.php',
 	            type:'POST',
 	            data :{
 	            'name':name,
